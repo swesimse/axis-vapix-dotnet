803c685b330c50b124a5ff0a69c66f9de1282366
Daniel Zarins <daniel@frazer.se>
2022-01-27 00:22:43 +0100

    First version, with one working request: GetAllDeviceProperties.

---
+++ AxisVapix/BasicDeviceInfo.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Swesim.Axis.Vapix
+{
+    internal class BasicDeviceRequest
+    {
+        [JsonProperty("apiVersion")]
+        public string ApiVersion;
+
+        [JsonProperty("context")]
+        public string Context;
+
+        [JsonProperty("method")]
+        public string Method;
+
+        public BasicDeviceRequest(double apiVersion, string context, string method)
+        {
+            this.ApiVersion = apiVersion.ToString("F1", CultureInfo.CreateSpecificCulture("en-US"));
+            this.Context = context;
+            this.Method = method;
+        }
+    }
+
+    public class BasicDeviceInfo
+    {
+        [JsonProperty("apiVersion")]
+        public double ApiVersion;
+
+        [JsonProperty("context")]
+        public string Context;
+
+        [JsonProperty("data")]
+        public DeviceData Data;
+
+        public class DeviceData
+        {
+            [JsonProperty("propertyList")]
+            public DeviceProperties Properties;
+
+            public class DeviceProperties
+            {
+                public string Architecture;
+                public string Brand;
+                public string BuildDate;
+                public string HardwareID;
+                public string ProdFullName;
+                public string ProdNbr;
+                public string ProdShortName;
+                public string ProdType;
+                public string ProdVariant;
+                public string SerialNumber;
+                public string Soc;
+                public string SocSerialNumber;
+                public string Version;
+                public string WebURL;
+            }
+        }
+    }
+}
---
+++ AxisVapix/VapixClient.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace Swesim.Axis.Vapix
+{
+    public class VapixClient
+    {
+        private IPAddress deviceIp;
+        private string username;
+        private string password;
+        private HttpClient httpClient;
+
+        public VapixClient(IPAddress deviceIp, string username, string password)
+        {
+            this.deviceIp = deviceIp;
+            this.username = username;
+            this.password = password;
+            var baseUri = new Uri($"http://{deviceIp}/axis-cgi/");
+            var credentialsCache = new CredentialCache();
+            credentialsCache.Add(baseUri, "Digest", new NetworkCredential(this.username, this.password));
+            var handler = new HttpClientHandler() { Credentials = credentialsCache, PreAuthenticate = true };
+            this.httpClient = new HttpClient(handler);
+            this.httpClient.BaseAddress = baseUri;
+        }
+
+        public BasicDeviceInfo GetAllDeviceProperties()
+        {
+            string jsonBody = JsonConvert.SerializeObject(new BasicDeviceRequest(1.0, "Client defined request ID", "getAllProperties"));
+            string result = SendVapixHttpRequest("basicdeviceinfo.cgi", HttpMethod.POST, jsonBody);
+            var resultObject = JsonConvert.DeserializeObject<BasicDeviceInfo>(result);
+            return resultObject;
+        }
+
+        private string SendVapixHttpRequest(string endPoint, HttpMethod method, string body = null)
+        {
+            if (!endPoint.EndsWith(".cgi"))
+                endPoint += ".cgi";
+
+            switch (method)
+            {
+                case HttpMethod.POST:
+                    return System.Text.Encoding.UTF8.GetString(httpClient.PostAsync(endPoint, new StringContent(body, System.Text.Encoding.UTF8)).Result.Content.ReadAsByteArrayAsync().Result);
+                case HttpMethod.GET:
+                    return httpClient.GetAsync(endPoint).Result.ToString();
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public enum HttpMethod
+        {
+            GET,
+            POST
+        }
+    }
+}
